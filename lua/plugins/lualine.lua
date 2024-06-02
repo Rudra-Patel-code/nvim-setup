1d9427a18bc1c16793819786585baeef9e3180af
Rudra Patel <rudra@kali-rudra>
2024-06-02 18:15:21 +0530

    intial commit

---
+++ lua/plugins/lualine.lua
@@ -0,0 +1,17 @@
+return {
+	{
+		"nvim-lualine/lualine.nvim",
+		dependencies = { "nvim-tree/nvim-web-devicons" },
+		config = function()
+			require("lualine").setup({
+				options = { theme = "dracula" },
+				sections = {
+					lualine_a = { "mode" },
+					lualine_b = { "branch" },
+					lualine_c = { "filename" },
+					lualine_x = { "diff" },
+				},
+			})
+		end,
+	},
+}
